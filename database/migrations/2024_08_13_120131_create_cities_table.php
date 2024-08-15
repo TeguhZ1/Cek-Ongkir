5611b125de1bb3b407d52233d99958dca1113cbd
AlfianNF <alfiannurfariq@gmail.com>
2024-08-15 16:50:56 +0700

    error select2

---
+++ database/migrations/2024_08_13_120131_create_cities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('code');
+            $table->unsignedBigInteger('province_code');
+            $table->string('title');
+            $table->timestamps();
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cities');
+    }
+};
---
+++ database/seeders/CitySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\City;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $fileKota = file_get_contents(base_path('database/kota.json'));
+        $fileKabupaten = file_get_contents(base_path('database/kabupaten.json'));
+
+        $dataKota = json_decode($fileKota, true);
+        $dataKabupaten = json_decode($fileKabupaten, true);
+
+        City::insert($dataKota);
+        City::insert($dataKabupaten);
+    }
+}
